4f12d618353598694909306f3aa3e22b3d45724a
Shivam <forgetpassward4@gmail.com>
2022-04-19 07:53:49 +0530

    Make faculty attedence details page and link it in home page

---
+++ FacultyAttendenceDetails.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class FacultyAttendenceDetails {
+    JFrame fr;
+    JButton b1;
+        
+    FacultyAttendenceDetails(){
+
+        fr = new JFrame("Faculty Attendence Details");
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        fr.setSize(screenSize.width, screenSize.height);
+        fr.setVisible(true);
+        
+        
+        
+        b1=new JButton("Print");
+        b1.setSize(100,200);
+        b1.setFont(new Font("Sans serif",Font.BOLD,25));
+        fr.add(b1,"South");
+    
+    }
+    
+    public static void main(String[] args){
+        new FacultyAttendenceDetails();
+    }    
+}
